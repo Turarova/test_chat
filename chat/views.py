1fb3e9faa65b7b7fe6b72a0492a993dbc9f05958
aigerim <maviboncuaika@gmail.com>
2022-08-22 15:19:25 +0600

    .

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+# from rest_framework.decorators import api_view
 
-# Create your views here.
+# @api_view(['POST'])
+# def test(request):

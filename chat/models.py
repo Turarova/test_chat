250c2612448a431cb9e5daa9ad270947f6115d28
bbch <bogda6ka.bogdan@gmail.com>
2022-08-22 14:52:17 +0600

    chat models

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+class Room(models.Model):
+    TYPES = [
+        ('GR', 'GROUP'),
+        ('DM', 'DIRECT'),
+    ]
+    user = models.ManyToManyField()
+    image = models.ImageField(null=True, upload_to='chat_images')
+    description = models.TextField(blank=True)
+    type = models.CharField(max_length=10, choices=TYPES)
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, related_name='messages')
+    room = models.ForeignKey(Room, related_name='messages')
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
--- config/settings.py
+++ config/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
 
     # libs
     'rest_framework',
-    'channels',
+    # 'channels',
 ]
 
 MIDDLEWARE = [

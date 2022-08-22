f6b0ebe2783d08aff3a74f73673e50877a010323
aigerim <maviboncuaika@gmail.com>
2022-08-22 16:16:09 +0600

    .

--- config/urls.py
+++ config/urls.py
@@ -23,7 +23,6 @@ urlpatterns = [
     path('test/', include('chat.urls'))
 ]
 
-urlpatterns += [
-    path(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
-    path(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
-]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+

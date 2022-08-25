cea765ed9820621922c012863967377340c67a54
bbch <bogda6ka.bogdan@gmail.com>
2022-08-25 13:53:02 +0600

    .

--- config/settings.py
+++ config/settings.py
@@ -85,16 +85,16 @@ ASGI_APPLICATION = 'config.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': config("DB_NAME"),
-#         'USER': config("DB_USER"),
-#         'PASSWORD': config("DB_PASSWORD"),
-#         'HOST': 'localhost',
-#         'PORT': 5432,
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config("DB_NAME"),
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASSWORD"),
+        'HOST': 'localhost',
+        'PORT': 5432,
+    }
+}
 
 
 
--- static/chat/js/send_msg.js
+++ static/chat/js/send_msg.js
@@ -1,16 +1,17 @@
 function send_msg() {
     console.log('yee')
-    let socket = new WebSocket('ws://127.0.0.1:8000/ws/');
+    let socket = new WebSocket('ws://127.0.0.1:8000/ws/chat/');
 
     socket.onopen = function(e) {
-        let info = {hello: "world", request_id: 2, action: 'post'};
+        // let info = {hello: "world", request_id: 2, action: 'create_message'};
+        let info = {action: 'create_message', message: 'heya', user: 1, request_id: 299, 'hren': 299};
         let json = JSON.stringify(info);
         console.log("DATA: ", json);
         socket.send(json);
       };
       
     socket.onmessage = function(event) {
-    console.log(`[message] Данные получены с сервера: ${event.data}`);
+        console.log(`[message] Данные получены с сервера: ${event.data}`);
     };
     
     socket.onclose = function(event) {
